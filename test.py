9eb20c1f70789db2c4453f7cccaa23de6229ad4c
capinho <papadiawara@hotmail.fr>
2021-12-14 15:35:47 +0000

    bug correct

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import schedule
 import time
 from datetime import datetime
+import requests 
 
 
 def dec(a):
@@ -23,7 +24,7 @@ def job():
 
 
 
-    r.html.render(timeout=30,sleep=10)
+    r.html.render(timeout=40,sleep=20)
 
     match_list = []
     print(r.status_code)
@@ -33,7 +34,7 @@ def job():
         cote2 = r.html.find("li:nth-of-type("+str(i)+") div:nth-of-type(2) li:nth-of-type(2) button",first=True)
         cote3 = r.html.find("li:nth-of-type("+str(i)+") div:nth-of-type(2) li:nth-of-type(3) button",first=True)
 
-        if ((float(cote1.text)==1.95 and endwith5(cote2.text)==0 and endwith5(cote3.text)==1) or (float(cote3.text)==1.95 and endwith5(cote2.text)==0 and endwith5(cote1.text)==0)):
+        if ((float(cote1.text)==1.95 and endwith5(cote2.text)==0 and endwith5(cote3.text)==0) or (float(cote3.text)==1.95 and endwith5(cote2.text)==0 and endwith5(cote1.text)==0)):
             requests.get("https://api.callmebot.com/whatsapp.php?phone=+221776206063&text=%s&apikey=534546" %(match.text))
         match_item = {
             'match':match.text,
